bb0de8bf9c7a3bbfc68f59ab1d41e8cfd8df99af
Michael <m4lehman@hsr.ch>
2016-11-23 01:34:57 -0800

    toby2 hinzugefügt

---
+++ src/drivers/toby/toby2.cpp
@@ -0,0 +1,441 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2013 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @file led.cpp
+ *
+ * LED driver.
+ */
+
+
+
+#include "toby2.h"
+#include <drivers/drv_toby.h>
+#include <drivers/device/ringbuffer.h>
+
+#include <stdio.h>
+#include <termios.h>
+
+#include <px4_config.h>
+#include <px4_posix.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <poll.h>
+#include <string.h>
+#include <fcntl.h>
+
+/*
+ * Ideally we'd be able to get these from up_internal.h,
+ * but since we want to be able to disable the NuttX use
+ * of leds for system indication at will and there is no
+ * separate switch, we need to build independent of the
+ * CONFIG_ARCH_LEDS configuration switch.
+ */
+
+//is not needed
+/*
+__BEGIN_DECLS
+extern void toby_init();
+__END_DECLS
+*/
+
+extern "C" __EXPORT int toby2_main(int argc, char *argv[]);
+
+
+/* Hilfs und Testfunktionen deklaration evt später als private in Klasse implementieren!*/
+
+int set_flowcontrol(int fd, int control);
+void *doClose(void *arg);
+int toby_init();
+void ringBufferTest();
+
+
+Toby2::Toby2() :
+#ifdef __PX4_NUTTX
+    CDev("Toby", "/dev/toby")
+#else
+    //Übernommen aus anderen Treiberimplementationen
+    VDev("toby", "/dev/toby")
+#endif
+{
+	// force immediate init/device registration
+	init();
+}
+
+Toby2::~Toby2()
+{
+}
+
+
+
+int Toby2::init()
+{
+    PX4_INFO("TOBY::init2");
+    ringBufferTest();   //Test
+    writeBuffer = new ringbuffer::RingBuffer(20,sizeof(char));
+#ifdef __PX4_NUTTX
+	CDev::init();
+#else
+	VDev::init();
+#endif
+    toby_init();
+
+	return 0;
+}
+
+
+int Toby2::open(device::file_t *filp){
+
+    //Debugging
+    PX4_INFO("open() is called");
+        pid_t x = ::getpid();
+        PX4_INFO("actual thread id : %d",x);
+
+
+    //*******************************************************************
+    int l = ::device::CDev::open(filp);
+    PX4_INFO("CDev:Open() mit return %d",l);
+
+
+
+    //***********************set options and open uart device************
+
+    //todo: Übernahme der Optionen termios des caller ... but how?
+    uart0_filestream =px4_open("/dev/ttyS6", O_RDWR |O_NOCTTY);
+
+    if(uart0_filestream == -1)
+    {
+        PX4_INFO("Unable to Open /dev/ttys6");
+
+    }
+    PX4_INFO("open return value /dev/ttys6: %d",uart0_filestream);
+
+    tcgetattr(uart0_filestream, &options);
+
+    //options.c_cflag &= ~(CSIZE | PARENB);
+    options.c_cflag = CS8;
+    //options.c_iflag = IGNPAR;
+    options.c_iflag&= ~(IGNBRK | BRKINT | ICRNL | INLCR | PARMRK | INPCK | ISTRIP | IXON);
+    options.c_oflag = 0;
+    //options.c_oflag = O_NONBLOCK;
+    options.c_lflag &= ~(ECHO | ECHONL | ICANON | IEXTEN | ISIG);
+    //options.c_lflag = ECHO;
+
+    options.c_cflag &= ~(CSTOPB | PARENB);
+
+    cfsetispeed(&options, B57600);
+    cfsetospeed(&options, B57600);
+
+
+  //  cfsetispeed(&options, B9600);
+  //  cfsetospeed(&options, B9600);
+
+    set_flowcontrol(uart0_filestream,0);
+    tcflush(uart0_filestream, TCIFLUSH);
+
+    if(tcsetattr(uart0_filestream, TCSANOW, &options)<0)
+    {
+        PX4_WARN("Wrong Options");
+    }
+
+
+   //Debugging :
+    union DeviceId deviceID =  CDev::Device::_device_id;
+    PX4_INFO("my DeviceID = %i", deviceID.devid);
+
+
+    //*******************************************************************
+
+
+    return l;
+}
+
+
+int	Toby2::close(device::file_t *filp){
+    PX4_INFO("close() is called, we close with %d",uart0_filestream);
+    ::device::CDev::close(filp);
+
+    //for Debugging
+    pid_t x = ::getpid();
+    PX4_INFO("actual thread id : %d",x);
+    //**************************************
+
+
+    tcflush(uart0_filestream, TCIFLUSH);
+
+    //force a thread to close the uart
+    pthread_t Toby_thread;
+    pthread_create(&Toby_thread, NULL, doClose, NULL);
+
+
+    //int i = set_flowcontrol(uart0_filestream,1);
+    //PX4_INFO("set_flowcontrol returns with %d");
+    //tcgetattr(uart0_filestream, &options);
+    //this->unlock();
+    //px4_close(uart0_filestream);
+
+    //for Debugging
+    PX4_INFO("toby::close() terminate");
+
+    //return value not valid yet!
+    return 0;
+}
+
+ssize_t	Toby2::read(device::file_t *filp, char *buffer, size_t buflen)
+{
+
+
+
+
+    //PX4_INFO("read() is called");
+    int i = px4_read(uart0_filestream,buffer,buflen);
+
+    return i;
+}
+
+ssize_t	Toby2::write(device::file_t *filp, const char *buffer, size_t buflen){
+
+    //todo : effizienter implementieren, but how?
+
+    //Debugging
+    // PX4_INFO("write() is called");
+
+    int count = 0;
+    if (uart0_filestream != -1)
+    {
+ //       PX4_INFO("::write() uart_filstream %d",uart0_filestream);
+
+
+        count = px4_write(uart0_filestream, buffer, buflen);
+        if (count < 0)
+        {
+            //Debugging
+            PX4_INFO("UART TX error");
+        }
+
+    }
+   // close(NULL);
+    return count;
+}
+
+
+off_t Toby2::seek(device::file_t *filp, off_t offset, int whence){
+    PX4_INFO("seek() is called");
+
+    return ::device::CDev::seek(filp,offset,whence);
+
+}
+
+
+
+
+int
+Toby2::ioctl(device::file_t *filp, int cmd, unsigned long arg)
+{
+    //PX4_INFO("ioctl mit cmd:  %d",cmd);
+    //ein versuch :
+    //int i = ::device::CDev::ioctl(filp,cmd,arg);
+    //PX4_INFO("ioctl() return %d",i);
+
+    //ioctl direct to uart
+    return ::ioctl(uart0_filestream,cmd,arg);
+}
+
+
+int	Toby2::poll(device::file_t *filp, struct pollfd *fds, bool setup){
+
+
+    px4_pollfd_struct_t fds1[1];
+    fds1[0].fd = uart0_filestream;
+    fds1[0].events = POLLIN;
+
+    int poll_return = px4_poll(fds1,1,500);
+    if(poll_return >0){
+        //notify the caller
+        poll_notify(POLLIN);
+        poll_notify_one(fds, POLLIN);
+    }
+
+    //  PX4_INFO("poll() is called with return %d",poll_return);
+
+
+    return 10;
+
+
+}
+
+
+
+
+
+int toby_init(){
+
+    //für Debuggingzwecke
+    PX4_INFO("toby_init");
+    return 0;
+
+    //todo : initalization toby L210 Module with AT-Command
+
+}
+
+namespace
+{
+Toby2 *gToby;
+}
+
+int
+toby2_main(int argc, char *argv[])
+{
+    /* set to default */
+    const char *device_name = TOBY_DEVICE_PATH;
+
+    //Load start parameter laden
+    int myoptind = 1;
+    const char *verb = argv[myoptind];
+
+
+    if (argc < 2) {
+
+        //stop test reset und status in planung
+        PX4_ERR("unrecognized command, try 'start', 'stop', 'test', 'reset' or 'status'");
+        PX4_ERR("[-d " TOBY_DEVICE_PATH "][-f (for enabling fake)][-s (to enable sat info)]");
+        return 1;
+    }
+
+    /*
+     * Start/load the driver.
+     */
+
+    if (!strcmp(verb, "start")) {
+        if (gToby != nullptr) {
+            warnx("already started");
+            return 1;
+        }
+
+
+        //"einmalige" Instanzierung
+        gToby = new Toby2();
+
+        return 0;
+    }
+
+
+    //todo : Alle input parameter handler implementieren
+    // Stubs für andere start-argumente
+    if (!strcmp(argv[1], "stop")) {
+    }
+
+    /*
+     * Test the driver/device.
+     */
+    if (!strcmp(argv[1], "test")) {
+    }
+
+    /*
+     * Reset the driver.
+     */
+    if (!strcmp(argv[1], "reset")) {
+    }
+
+    /*
+     * Print driver status.
+     */
+    if (!strcmp(argv[1], "status")) {
+    }
+
+    if(device_name){
+
+    }
+
+
+    //return value is not valid yet
+    return 0;
+
+
+}
+
+
+//****************************hilfsfunktionen********************************
+
+void *doClose(void *arg)
+{
+
+
+    PX4_INFO("Thread started");
+    pid_t x = ::getpid();
+    PX4_INFO("actual thread id : %d",x);
+    int i = px4_close(4);
+
+    PX4_INFO("Thread closed Uart with %d",i);
+
+
+    return NULL;
+
+
+}
+
+int set_flowcontrol(int fd, int control)
+{
+    struct termios tty;
+    memset(&tty, 0, sizeof tty);
+    if (tcgetattr(fd, &tty) != 0)
+    {
+        perror("error from tggetattr");
+        return -1;
+    }
+
+    if(control) tty.c_cflag |= CRTSCTS;
+    else tty.c_cflag &= ~CRTSCTS;
+
+    if (tcsetattr(fd, TCSANOW, &tty) != 0)
+    {
+        perror("error setting term attributes");
+        return -1;
+    }
+    return 0;
+}
+
+void ringBufferTest(){
+
+
+    unsigned char tx_buffer[]={"Hallo Michael"};
+
+    ringbuffer::RingBuffer *x = new ringbuffer::RingBuffer(10,sizeof(char));
+    x->put('h');
+
+
+    x->get(tx_buffer[1]);
+
+    PX4_INFO("Received: %s", tx_buffer);
+}
+
